ef5e3d896ce62d87f0d655e82cde5870df7f3332
Mati Sediqi <mati_01@icloud.com>
2022-03-19 12:05:55 +0000

    build: removed re-defining DEBUG_MODE const since its already by default false

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import Cookiebox from './Cookiebox';
 
 export const config = (...filters: CookieSource[]): Cookiebox => {
     const cookiebox = new Cookiebox();
-    cookiebox.DEBUG_MODE = false;
 
     for (const filter of filters) {
         cookiebox.addSourceFilter(filter);
